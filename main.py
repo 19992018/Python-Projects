7cd92393879620bb6437d7bd6f6b72d0411aa194
Margret Mauno <36762352+19992018@users.noreply.github.com>
2024-03-08 16:12:32 +0300

    Total and Average
    Total and average per student calculated and added to the dictionary which is storing data

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def handle_student_details():
         one_student["name"]=name
         students_details[id_no] = one_student
         print(students_details)
+        print(" ")
 
         count = count + 1
 
@@ -27,6 +28,7 @@ def handle_subjects():
         subject = input("Key in subject: ")
         subjects.append(subject)
         print(subjects)
+        print(" ")
         count = count+1
 
     # for x in estvalue:
@@ -35,11 +37,20 @@ def handle_subjects():
     # print("NEW VALUE IS: ", estvalue)
 
     for x in estvalue:
+        add=0
         for subject in subjects:
             mark = input("Key in " + subject + " mark for student with id " + x + " ")
             estvalue[x][subject] = mark
+            add = add+int(mark)
 
+        print("total for id:" + x + " =" + str(add))
+        avg = int(add)/5
+        print("average for id:" + x + " =" + str(avg))
+        print(" ")
+        estvalue[x]["total"] = add
+        estvalue[x]["average"] = add/5
     print("NEW VALUE IS: ", estvalue)
+
 
 
 handle_subjects()
